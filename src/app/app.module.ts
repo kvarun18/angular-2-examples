d020d12f9c6f3d27fb9c2928bdf8661852ed228a
kaja varun <kaja varun>
2016-10-28 09:24:26 -0400

    changes in appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { HelloModule } from './hello/hello.module';
-
+import { ExamplesModule } from './examples/examples.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +10,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    HelloModule,
+    ExamplesModule,
   ],
   declarations: [
     AppComponent,
